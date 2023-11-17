3d4fcd1481ad54b3d807edceaacab0a2657263fa
bakorx <asnkwamebakor@gmail.com>
2023-11-17 10:07:39 +0000

    separated js code from html script

---
+++ cs-website.js
@@ -0,0 +1,33 @@
+ let currentSlide = 0;
+     const slides = document.querySelectorAll('.slide');
+        
+         function showSlide(index) {
+            if (index < 0) {
+                    currentSlide = slides.length - 1;
+             } else if (index >= slides.length) {
+                currentSlide = 0;
+            } else {
+                currentSlide = index;
+               }
+        
+                
+             slides.forEach((slide) => {
+                slide.style.display = 'none';
+            });
+        
+               
+            slides[currentSlide].style.display = 'block';
+            }
+        
+            function prevSlide() {
+                showSlide(currentSlide - 1);
+            }
+        
+            function nextSlide() {
+                showSlide(currentSlide + 1);
+            }
+        
+            
+            showSlide(currentSlide);
+
+        
